5417ebae176bda1e9d6c6dab5a94ede4d97e489a
Thierry Tranchina <akindofceasar@gmail.com>
2015-09-16 13:43:03 +0200

    speed is keyboard controlled

--- engine/interface.js
+++ engine/interface.js
@@ -4,10 +4,11 @@
 	var data = {
 		displayed: 'home'
 	};
-	var el;
-	var rw;
-	var timer = 0;
-	var el_timer;
+	var el, rw;
+	var speed_list 	= [1, 5, 20, 30, 200],
+		speed_id 	= 2,
+		timer 		= 0;
+	var el_timer, el_speed;
 
 	var interface = {
 
@@ -29,6 +30,7 @@
 			
 			timer++;
 			el_timer.innerHTML = timer;
+			el_speed.innerHTML = 5-speed_id;
 		},
 		
 
@@ -47,6 +49,23 @@
 					log('[interface] space');
 					break;
 
+				// UP
+				case 38:
+					rw.config.timer = 0;
+					speed_id--;
+					if(speed_id<0)	speed_id = 0;
+					rw.config.time_step = speed_list[speed_id];
+					log('[interface] speed up : '+rw.config.time_step);
+					break;
+
+				// DOWN
+				case 40:
+					rw.config.timer = 0;
+					speed_id++;
+					if(speed_id>=speed_list.length)	speed_id = speed_list.length-1;
+					rw.config.time_step = speed_list[speed_id];
+					log('[interface] speed down : '+rw.config.time_step);
+					break;
 
 			}
 
@@ -67,13 +86,14 @@
 
 	function build_dom(){
 
-		infos = "<h3 id='timer'>"+timer+"</h3>";
+		infos = "<h3><span  id='timer'>"+timer+"</span></h3><h3 class='speed'><span id='speed'></span>/5</h4>";
 		el = get_element('div', 'interface', {
 			innerHTML: infos
 		});
 		rw.arena.graphic.container.appendChild( el );
 
 		el_timer = document.getElementById('timer');
+		el_speed = document.getElementById('speed');
 	}
 
 })(robotWalken);
